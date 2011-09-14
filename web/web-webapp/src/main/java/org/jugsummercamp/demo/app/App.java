f1742ca1c5962a7d6dece6331b2c9d093f9174e6
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-09-14 18:00:43 +0200

    Fixes + add remote console access

--- web/web-webapp/src/main/java/org/jugsummercamp/demo/app/App.java
+++ web/web-webapp/src/main/java/org/jugsummercamp/demo/app/App.java
@@ -1,5 +1,7 @@
 package org.jugsummercamp.demo.app;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.jboss.weld.environment.osgi.api.events.Invalid;
 import org.osgi.util.tracker.ServiceTracker;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -7,9 +9,7 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
-import java.util.concurrent.atomic.AtomicBoolean;
 import static org.jboss.weld.environment.osgi.api.events.BundleContainerEvents.*;
-import org.jboss.weld.environment.osgi.api.events.Invalid;
 import org.jboss.weld.environment.osgi.api.events.Valid;
 import org.jugsummercamp.demo.fwk.HttpServiceTracker;
 
@@ -21,7 +21,20 @@ public class App {
     @Inject @Any Instance<Object> instances;
         
     private ServiceTracker tracker;
+    
     private AtomicBoolean valid = new AtomicBoolean(false);
+
+    public void validate(@Observes Valid event) {
+        valid.getAndSet(true);
+    }
+
+    public void invalidate(@Observes Invalid event) {
+        valid.getAndSet(false);
+    }
+    
+    public boolean isValid() {
+        return valid.get();
+    }
 
     public void start(@Observes BundleContainerInitialized init) throws Exception {
         this.tracker = new HttpServiceTracker(
@@ -29,17 +42,5 @@ public class App {
                 getClass().getClassLoader(), 
                 instances, CONTEXT_ROOT);
         this.tracker.open();
-    }
-    
-    public void validate(@Observes Valid event) {
-        valid.compareAndSet(false, true);
-    }
-
-    public void invalidate(@Observes Invalid event) {
-        valid.compareAndSet(true, false);
-    }
-
-    public boolean isValid() {
-        return valid.get();
     }
 }
